40c89741ca5a118f81cb41fcd99d149af1aa9df8
sejoung kim <sejoung@gmail.com>
2025-02-28 18:55:20 +0900

    리팩토링

--- src/sejoung/configuration/container.py
+++ src/sejoung/configuration/container.py
@@ -1,19 +1,16 @@
-from dependency_injector import containers
+from dependency_injector import containers, providers
+
+from sejoung.configuration.database import Database
+from sejoung.repositories.user import UserRepository
 
 
 class Container(containers.DeclarativeContainer):
-    wiring_config = containers.WiringConfiguration(modules=["sejoung.controller.user"])
+    wiring_config = containers.WiringConfiguration(modules=["sejoung.application"])
 
     config = providers.Configuration(yaml_files=["config.yml"])
 
     db = providers.Singleton(Database, db_url=config.db.url)
-
     user_repository = providers.Factory(
         UserRepository,
         session_factory=db.provided.session,
-    )
-
-    user_service = providers.Factory(
-        UserService,
-        user_repository=user_repository,
     )
