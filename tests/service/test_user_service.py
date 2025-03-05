e5e760c72462c2c48e6fb3fd921900e99b4e3160
sejoung kim <sejoung@gmail.com>
2025-03-05 14:14:24 +0900

    service 테스트 코드 추가

--- tests/service/test_user_service.py
+++ tests/service/test_user_service.py
@@ -6,9 +6,21 @@ from sejoung.service.user_service import UserService
 
 
 @pytest.mark.asyncio
-async def test_user_service(session, generate_uuid):
+async def test_user_service_not_found(session, generate_uuid):
     repository = UserRepository(session)
     service = UserService(repository)
 
     with pytest.raises(UserNotFoundError):
         await service.get_user(generate_uuid)
+
+
+@pytest.mark.asyncio
+async def test_user_service_found(session):
+    repository = UserRepository(session)
+    service = UserService(repository)
+    create_user = await repository.create("zolla@abc.com", "beni")
+
+    actual = await service.get_user(create_user.id)
+
+    assert actual.email == "zolla@abc.com"
+    assert actual.name == "beni"
