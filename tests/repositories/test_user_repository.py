1f8b9f0ca8317acaa0250fe38c2476f0ed41d06c
sejoung kim <sejoung@gmail.com>
2025-03-06 15:29:45 +0900

    테스트 수행

--- tests/repositories/test_user_repository.py
+++ tests/repositories/test_user_repository.py
@@ -4,7 +4,7 @@ from sejoung.repositories import get_user_repository
 
 
 @pytest.mark.asyncio
-async def test_user_repository(setup,generate_uuid):
+async def test_user_repository(app,generate_uuid):
     repository = get_user_repository()
     await repository.find_all()
     actual = await repository.find_one(generate_uuid)
@@ -12,7 +12,7 @@ async def test_user_repository(setup,generate_uuid):
 
 
 @pytest.mark.asyncio
-async def test_create_user(setup):
+async def test_create_user(app):
     repository = get_user_repository()
     user = await repository.create("zolla@abc.com", "beni")
     assert user.email == "zolla@abc.com"
