542c75e6a083174c9c1999d0628cded80527b4da
sejoung kim <sejoung@gmail.com>
2025-03-06 15:22:29 +0900

    실글턴 롤백

--- src/sejoung/configuration/database.py
+++ src/sejoung/configuration/database.py
@@ -10,23 +10,21 @@ from .logger import log
 
 
 class Database:
-    __instance = None
 
     def __init__(self, db_url: str) -> None:
         if db_url is None:
             raise ValueError("db_url is required")
         log.debug("db_url %s", db_url)
-        if Database.__instance is None:
-            self.__engine = create_async_engine(
-                db_url,
-                echo=True,
-                echo_pool=True,
-                pool_recycle=60,
-                pool_size=1,
-                max_overflow=5,
-                pool_timeout=50,
-                isolation_level="READ COMMITTED",
-            )
+        self.__engine = create_async_engine(
+            db_url,
+            echo=True,
+            echo_pool=True,
+            pool_recycle=60,
+            pool_size=1,
+            max_overflow=5,
+            pool_timeout=50,
+            isolation_level="READ COMMITTED",
+        )
 
     async def create_database(self) -> None:
         async with self.__engine.begin() as conn:
@@ -46,9 +44,3 @@ class Database:
 
     def __del__(self):
         asyncio.run(self.__engine.dispose())
-
-    @classmethod
-    def get_instance(cls, db_url: str) -> "Database":
-        if not cls.__instance:
-            cls.__instance = Database(db_url)
-        return cls.__instance
